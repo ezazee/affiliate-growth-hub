f30f7d408e53b9c61cf2201ef13177245b4ac3ae
KBN Dev <yukiirima123@gmail.com>
2026-01-10 03:37:57 +0700

    chore: remove .next from git tracking

--- .next/dev/static/chunks/src_app_affiliator_page_tsx_5f6a8242._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_fc45b05d._.js"
-],
-    source: "dynamic"
-});
